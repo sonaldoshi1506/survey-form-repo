038600f6eb9e48be69a553a1e948bd8083525a6f
manandoshi1607 <manan.doshi@spit.ac.in>
2020-06-03 18:03:20 +0530

    Added build modules

---
+++ build/precache-manifest.f9bb794353ba9b483f542565bcb36c47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baae2506ef08610ac2d78246c07ddcfb",
+    "url": "/survey-form/index.html"
+  },
+  {
+    "revision": "cbe2c9bc4aadf2b28d4c",
+    "url": "/survey-form/static/css/main.5346f03d.chunk.css"
+  },
+  {
+    "revision": "a40a6432d2f17b8e9b2d",
+    "url": "/survey-form/static/js/2.7c9845f5.chunk.js"
+  },
+  {
+    "revision": "6dbdd4c517706a59de5c17452b75352e",
+    "url": "/survey-form/static/js/2.7c9845f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbe2c9bc4aadf2b28d4c",
+    "url": "/survey-form/static/js/main.271e81d1.chunk.js"
+  },
+  {
+    "revision": "054fa5d78ade1ad7ac6f",
+    "url": "/survey-form/static/js/runtime-main.ff9970de.js"
+  }
+]);
